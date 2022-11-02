cb13d85fcbbd18c5bef4c90293f41bb9ebf13385
mukul442 <61179549+mukul442@users.noreply.github.com>
2022-11-02 11:25:18 +0000

    2 files modified

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
   var elements = this.elements;
   for (var i = 0; i < elements.length - 1; i++) {
-    console.log(elements[i]);
+    if (elements[i].value === '' || elements[i].value == null) {
+      elements[i].classList.add('error');
+    } else {
+      elements[i].classList.remove('error');
+    }
   }
 });
