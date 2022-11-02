decdb0d43d2dfc82266f9ea0ec6e02c2f1e48b27
mukul442 <61179549+mukul442@users.noreply.github.com>
2022-11-02 11:23:08 +0000

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+var form = document.getElementById('form');
+
+form.isValidated = true;
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var elements = this.elements;
+  for (var i = 0; i < elements.length - 1; i++) {
+    console.log(elements[i]);
+  }
+});
